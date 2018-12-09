cc94d8e456c82df25cd7961752ad9e76d21a705d
Ckbhatia <chetansain86@gmail.com>
2018-12-09 17:26:22 +0530

    feat: added onlClick to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,8 @@ class Button extends React.Component {
 	render() {
 		return (
 			<button 
-				className={ this.props.light ? 'light-button' : 'dark-button' }>
+				className={ this.props.light ? 'light-button' : 'dark-button' }
+                onClick={this.props.onClick}>
 				Refresh
 			</button>
 		);
